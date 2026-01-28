ef625f722ae7cc129c7d50df322dded256560ac0
First-Git <saurabhlalwani495@gmail.com>
2026-01-28 22:13:20 +0530

    Deploy React app to production

--- backend/app.py
+++ backend/app.py
@@ -195,5 +195,5 @@ async def http_exception_handler(request: Request, exc: HTTPException):
 
 if __name__ == "__main__":
     import uvicorn
-    port = int(os.getenv("PORT", 5000))
-    uvicorn.run("app:app", host="0.0.0.0", port=port, reload=False)
+    port = int(os.getenv("PORT", 10000))
+    uvicorn.run("app:app", host="0.0.0.0", port=port)
